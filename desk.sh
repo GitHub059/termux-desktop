75cef4b1405b400ceca687fa3ee60423b0dc8233
GitHub059 <97761167+GitHub059@users.noreply.github.com>
2022-01-20 13:37:19 -0300

    Create desk.sh

---
+++ desk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "está a intalar,aguarde"
+apt install wget tar zip
+echo "agora é o desk."
+wget(
+unzip ~/ubuntu-termux.zip
+cd ~/ubuntu-termux
+tar -xvzf(  /data/data/com.termux/files/
+echo "já foi finalizado agora é só reiniciar, e digitar-> pc <-e se divertir,
+basta digitar vncserver^_^"
