13e0590cc30a9ede32af5c9e277041304fa77c39
GitHub059 <97761167+GitHub059@users.noreply.github.com>
2022-01-20 14:47:24 -0300

    Update desk.sh

--- desk.sh
+++ desk.sh
@@ -7,6 +7,9 @@ echo "agora é o desk."
 acho "isso pode demorar um pouco, tenha paciência"
 echo "se estiver pronto ou afim press enter.ou ctrl ^C para cancelar"
 read enter
+exec termux-setup-storage
+echo "enter"
+read enter
 curl -L -o ubuntu-termux.zip https://download2263.mediafire.com/zcyethh81pvg/w5bbi9uy2mhyt1h/termux-backup.tgz
 unzip ubuntu-termux.zip
 cd ubuntu-termux
