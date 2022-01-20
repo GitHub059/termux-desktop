9c82f1436323f4b507d1c7363ccb8cceeecb1577
GitHub059 <97761167+GitHub059@users.noreply.github.com>
2022-01-20 14:56:05 -0300

    Update desk.sh

--- desk.sh
+++ desk.sh
@@ -7,12 +7,10 @@ echo "agora é o desk."
 echo "isso pode demorar um pouco, tenha paciência"
 echo "se estiver pronto ou afim press enter.ou ctrl ^C para cancelar"
 read enter
-exec termux-setup-storage
-echo "enter"
-read enter
 curl -L -o ubuntu-termux.zip https://download2263.mediafire.com/zcyethh81pvg/w5bbi9uy2mhyt1h/termux-backup.tgz
 unzip ubuntu-termux.zip
 cd ubuntu-termux
 tar -xvzf termux-backup.tgz /data/data/com.termux/files/
 echo "já foi finalizado agora é só reiniciar, e digitar-> pc <-e se divertir,
 basta digitar vncserver^_^"
+exec termux-setup-storage
