0bff58ef8f8fccf7752832a41a483bdaf227c692
GitHub059 <97761167+GitHub059@users.noreply.github.com>
2022-01-24 12:19:11 -0300

    Update desk.sh

--- desk.sh
+++ desk.sh
@@ -14,7 +14,7 @@ echo "########>"
 curl -L -o ubuntu-termux.zip https://download2263.mediafire.com/zcyethh81pvg/w5bbi9uy2mhyt1h/termux-backup.tgz
 echo "########>"
 cd ..
-tar -xvzf ~/ubuntu-termux.zip 
+tar -xvzf ubuntu-termux.zip /data/data/com.termux/files/
 echo "já foi finalizado agora é só reiniciar, e digitar-> pc <-e se divertir,
 basta digitar vncserver^_^"
 
